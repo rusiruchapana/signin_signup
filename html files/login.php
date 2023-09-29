8a0f5196a67e71af3fc3fc809e1a7c2f23fa3f99
rusiruchapana <rusiruchapana@gmail.com>
2023-09-29 12:47:16 +0530

    i coded to login page for search data in the database and login for the homepage

--- html files/login.php
+++ html files/login.php
@@ -1,4 +1,47 @@
+<?php
+    include("DB_connect.php");
 
+    $errors2 = array("username"=>"", "password"=>"");
+    $username2;
+    $password2;
+
+    if(isset($_POST["login_login_btn"])){
+        //to find errors in the fields
+        if(empty($_POST["username"])){
+            $errors2["username"] = "Username field is empty!.";
+        }else{
+            $username2 = $_POST["username"];
+        }
+
+        if(empty($_POST["password"])){
+            $errors2["password"] = "Password field is empty!.";
+        }else{
+            $password2 = $_POST["password"];
+        }
+
+        //for login part
+        if(!array_filter($errors2)){
+            $sql = "select username,password from operation where username ='$username2' and password ='$password2' ";
+            $result2 = mysqli_query($conn, $sql);
+
+            if($result2){
+
+                if(mysqli_num_rows($result2)==1){
+                    header("Location: home.php");
+                }else{
+                    //login error
+                }
+
+            }else{
+                echo "Database not connected!." . mysqli_connect_error();
+            }
+
+        }else{
+
+        }
+    }
+
+?>
 
 
 
@@ -34,18 +77,19 @@
 <body>
     <div class="main">
             <div>
-                <form action="">
+                <form action="login.php" method="POST">
                     <h1 class="title">Login page</h1>
         
                     <label for="username">Enter username:</label>
-                    <input type="text" name="username" id="username"><br><br>
+                    <input type="text" name="username" id="username"><br>
+                    <div style="color: red"><?php echo $errors2["username"] ?></div><br>
         
                     <label for="password">Enter password:</label>
-                    <input type="password" name="password" id="password"><br><br>
-        
+                    <input type="password" name="password" id="password"><br>
+                    <div style="color: red"><?php echo $errors2["password"] ?></div><br>
                    
                    <button class="signup_btn"><a href="signup.php">Signup</a></button>
-                   <button class="login_btn">login</button>
+                   <button class="login_btn" name="login_login_btn">login</button>
                    
         
         
